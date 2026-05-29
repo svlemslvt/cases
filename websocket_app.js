654ce80dd1b26816763458e5c420514d30690361
RAMZES <godofplugg@proton.me>
2026-05-29 18:19:30 +0500

    Add files via upload

---
+++ websocket_app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (data) => {
+        io.emit('chat message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Listening on *:3000');
+});
+
